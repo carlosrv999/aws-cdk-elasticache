8252409493c99e69d06a11e2280c20cd9b5f459f
Carlos Ramirez <carlosrv125@icloud.com>
2023-01-19 21:44:47 -0500

    finished docker

--- app/index.js
+++ app/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import { createClient } from "redis";
 const app = express();
 const port = 3000;
+const redis_host = process.env.REDIS_HOST;
+const redis_port = process.env.REDIS_PORT;
 
 const connectRedis = async () => {
-  const client = createClient();
+  const client = createClient({ url: `redis://${redis_host}:${redis_port}` });
   client.on("error", (err) => console.log("Redis Client Error", err));
   await client.connect();
   await client.set("key", "value");
